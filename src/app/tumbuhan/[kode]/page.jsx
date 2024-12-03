99dca4c61ee720a61ee7ac8f09e58cca24ca54a9
Andrea Francesca <shelbylimitcompany@gmail.com>
2024-12-03 11:01:40 +0700

    Update page.jsx

--- src/app/tumbuhan/[kode]/page.jsx
+++ src/app/tumbuhan/[kode]/page.jsx
@@ -45,7 +45,7 @@ export default function Tumbuhan({ params }) {
     <div className="p-4 w-full h-full bg-black justify-center items-center ">
       <div className="overflow-hidden border border-white w-full max-h-screen lg:h-full rounded-lg">
         <div className="flex flex-col lg:flex-row">
-          <div className="relative h-full w-full lg:w-2/3 lg:h-auto">
+          <div className="relative h-full w-full lg:w-2/3 border-b lg:h-auto">
             <img
               src={item.foto}
               className="w-full h-[250px] lg:h-full lg:w-full bg-cover bg-center rounded-t-md"
@@ -60,9 +60,9 @@ export default function Tumbuhan({ params }) {
           <div className="flex-1 relative overflow-hidden justify-center w-full h-full items-center">
             <img
               src="https://res.cloudinary.com/dgnfgxqem/image/upload/v1732966875/tumbuhan/kgt0pkfa1jjyqfx1mill.jpg"
-              className="w-full h-full lg:h-full object-cover rounded-t-md"
-            />
-            <div className="overflow-hidden absolute bg-black bg-opacity-50 top-0 left-1/2 transform -translate-x-1/2 rounded-b-lg  max-lg:rounded-r-none w-full h-full px-2 text-xs lg:max-h-screen">
+              className="w-full h-full lg:h-full object-cover"
+            />rounded
+            <div className="overflow-hidden absolute bg-black bg-opacity-50 top-0 left-1/2 transform -translate-x-1/2 rounded-b-lg max-lg:rounded-r-none w-full h-full px-2 text-xs lg:max-h-screen">
             <div className='text-white mx-2 px-2 text-justify h-full lg:h-full overflow-y-auto no-scrollbar'>
               <div className="font-bold mt-2"> {item.nama} : {item.subNama}</div>
               <div className=" mt-1">{item.namaLatin}</div>
